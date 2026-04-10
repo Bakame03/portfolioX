0ac90bdb2bbf89719b9f69a925996228005ffb66
bakame03 <nganjialdoalex@gmail.com>
2026-04-10 12:55:37 +0200

    feat: add navigation arrows to projects slider with responsive styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -229,6 +229,10 @@
           el: '.swiper-pagination',
           clickable: true,
         },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
         keyboard: {
           enabled: true,
         },
